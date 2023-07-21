ddcd7c7301c18e8a6bdc16b419b7e1c32e9d1725
eQereum <97484374+eQereum@users.noreply.github.com>
2023-07-21 06:27:45 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8f5b28c6fd9ad70a136eedf42409a14fc028a816";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
